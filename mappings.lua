15c640fb6a3c51d4a0ca5ffcb8af1ea3d7f5e6d4
zisakmc <mishaela98@gmail.com>
2023-09-19 15:29:23 +0530

    settings

--- mappings.lua
+++ mappings.lua
@@ -32,7 +32,6 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    ["<leader>uT"] = { desc = "theme" },
   },
   t = {
     -- setting a mapping to false will disable it
